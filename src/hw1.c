1d6a0d2aeae01472c46572a6bdd67c766df8f17c
Zoheb Hasan <106490884+ZohebHasan@users.noreply.github.com>
2024-02-13 03:32:25 +0000

    Beta Test 1:  Section 1

--- src/hw1.c
+++ src/hw1.c
@@ -1,9 +1,201 @@
 #include "hw1.h"
 
-void print_packet_sf(unsigned char packet[])
-{
-    (void)packet;
+
+//Global Variable Declarations 
+unsigned int sourceAddress; 
+unsigned int destinationAddress; 
+unsigned int sourcePort;
+unsigned int destinationPort; 
+unsigned int fragmentOffset; 
+unsigned int packetLength; 
+unsigned int maxHopCount;
+unsigned int checkSum;
+unsigned int compressionScheme;
+unsigned int trafficClass;
+
+//Function Prototypes
+void decomposePayload(unsigned char packet[], int* payload, int payloadLength);
+int getPayloadLength(unsigned char packet[]);
+void decomposeHeader(unsigned char packet[]);
+unsigned int getPacketLength(unsigned char packet[]);
+void print_packet_sf(unsigned char packet[]);
+
+int getPayloadLength(unsigned char packet[]){
+    int pktLen = (int) getPacketLength(packet); 
+    int val = pktLen- 16;
+    return ((int) ((val) / 4)) + (val % 4 != 0); 
+}  
+
+
+unsigned int getPacketLength(unsigned char packet[]){
+    unsigned int temp = 0;
+
+    for(int i = 9; i < 12; i++){
+        if(i == 9){
+            temp |= (unsigned int) (packet[i] & 0x03); 
+        }
+        else if( i == 11){
+            temp |= (unsigned int) packet[i]; 
+            temp >>= 4;         
+        }
+        else {
+            temp |= (unsigned int) packet[i]; 
+            temp <<= 8;        
+        }      
+    }
+    packetLength = temp; 
+    temp = 0; 
+
+
+    return packetLength;
 }
+
+
+void decomposeHeader(unsigned char packet[]){ 
+    getPacketLength(packet);
+    unsigned int temp = 0; 
+    int length = 16;
+    
+    for(int i = 0; i < length; i++){  //what if the length is corrupted? gotta check pfft.
+        if( i < 4) { 
+            temp |= (unsigned int) packet[i];            
+            if( i == 3){
+                temp >>= 4;
+                sourceAddress = temp; 
+                temp = 0;           
+            }
+            else{
+                temp <<= 8;
+            }     
+        }
+        else if( i < 7 ){  
+            if(i == 3){
+               temp |= (unsigned int) (packet[i] & 0x0f);   
+            }
+            else{
+                temp |= (unsigned int) packet[i];
+            }
+            if( i < 6){
+                temp <<= 8;
+            }
+            else{
+                destinationAddress = temp;
+                temp = 0; 
+            }           
+        }
+        else if(i == 7){
+            temp = packet[i];
+            temp >>= 4;
+            sourcePort = temp;
+            temp = 0;
+            temp |= (unsigned int) (packet[i] & 0x0f);  
+            destinationPort = temp;
+            temp = 0;
+        } 
+        else if(i < 10){ //needs recheck with different data 
+            temp |= (unsigned int) packet[i];
+            if( i == 9){
+                temp >>= 2;
+                fragmentOffset = temp; 
+                temp = 0;
+            }
+            else{
+                temp <<= 8;   
+            }
+        }
+        else if(i < 11){
+            continue;
+        }
+        else if( i < 13){
+            if( i == 11){
+                temp |= (unsigned int) (packet[i] & 0x0f);
+                temp <<= 8;
+            }
+            else{
+                temp |= (unsigned int) (packet[i] & 0x80);
+                temp >>= 7; 
+                maxHopCount = temp;
+                temp = 0;
+                temp |= (unsigned int) (packet[i] & 0x7f);
+                temp <<= 8;          
+            }
+        }
+        else if( i < 15){
+            temp |= (unsigned int) packet[i];                   
+            if( i == 14){     
+                checkSum = temp;                
+                temp = 0;
+            }
+            else{
+                temp <<= 8;  
+            }
+        }
+        else if( i == 15){
+            temp |= (unsigned int) (packet[i] & 0xC0);
+            temp >>= 6; 
+            compressionScheme = temp;
+            temp = 0;
+            temp |= (unsigned int) ( packet[i] & 0x3F);
+            trafficClass = temp;
+            temp = 0;     
+        }
+    }
+ }
+
+
+void decomposePayload(unsigned char packet[], int* payload, int payloadLength){
+    unsigned int temp = 0; 
+    int pktLen = (int) getPacketLength(packet);
+    int payLoadLength = getPayloadLength(packet);
+  
+   
+    if(payLoadLength != 0){
+        int payloadIndex = 0, bitCount = 0;
+        for(int i = 16; i < pktLen; i++){ //tentative algorithm. POTENTIAL VULNERABILITIES (Not checking checksum or looking for corrupted data)  
+            temp |= packet[i];
+            
+            bitCount+=8; 
+            if(bitCount == 32){
+                payload[payloadIndex] = (int) temp; 
+                temp = 0;
+                payloadIndex++;
+                bitCount = 0;
+            }
+            else {
+                temp <<= 8;
+            }
+        }
+    }
+}
+
+
+void print_packet_sf(unsigned char packet[]){
+
+    int pktLen = (int) getPacketLength(packet);
+    int payLoadLength = getPayloadLength(packet);
+    int payload[payLoadLength]; 
+    decomposeHeader(packet);
+    decomposePayload (packet, payload, payLoadLength);
+
+    printf("Source Address: %u\n", sourceAddress);
+    printf("Destination Address: %u\n", destinationAddress); 
+    printf("Source Port: %u\n", sourcePort); 
+    printf("Destination Port: %u\n", destinationPort); 
+    printf("Fragment Offset: %u\n", fragmentOffset); 
+    printf("Packet Length: %u\n", packetLength); 
+    printf("Max Hop Count: %u\n", maxHopCount); 
+    printf("CheckSum: %u\n", checkSum); 
+    printf("Compression Scheme: %u\n", compressionScheme); 
+    printf("Traffic Class: %u\n", trafficClass);   
+
+    printf("Payload: "); 
+    for(int i = 0; i < payLoadLength; i++){
+        printf("%d ", payload[i]);
+    }
+    printf("\n"); 
+   
+}
+
 
 unsigned int compute_checksum_sf(unsigned char packet[])
 {
--- src/hw1_main.c
+++ src/hw1_main.c
@@ -1,7 +1,213 @@
 #include <stdio.h>
-#include "hw1.h"
+// #include "hw1.h"
+
+
+unsigned int sourceAddress; 
+unsigned int destinationAddress; 
+unsigned int sourcePort;
+unsigned int destinationPort; 
+unsigned int fragmentOffset; 
+unsigned int packetLength; 
+unsigned int maxHopCount;
+unsigned int checkSum;
+unsigned int compressionScheme;
+unsigned int trafficClass;
+
+void decomposePayload(unsigned char packet[], int* payload, int payloadLength);
+int getPayloadLength(unsigned char packet[]);
+void decomposeHeader(unsigned char packet[]);
+unsigned int getPacketLength(unsigned char packet[]);
+void print_packet_sf(unsigned char packet[]);
+
+int getPayloadLength(unsigned char packet[]){
+    int pktLen = (int) getPacketLength(packet); 
+    int val = pktLen- 16;
+    return ((int) ((val) / 4)) + (val % 4 != 0); 
+}  
+
+
+unsigned int getPacketLength(unsigned char packet[]){
+    unsigned int temp = 0;
+
+    for(int i = 9; i < 12; i++){
+        if(i == 9){
+            temp |= (unsigned int) (packet[i] & 0x03); 
+        }
+        else if( i == 11){
+            temp |= (unsigned int) packet[i]; 
+            temp >>= 4;         
+        }
+        else {
+            temp |= (unsigned int) packet[i]; 
+            temp <<= 8;        
+        }      
+    }
+    packetLength = temp; 
+    temp = 0; 
+
+
+    return packetLength;
+}
+
+
+void decomposeHeader(unsigned char packet[]){ 
+    getPacketLength(packet);
+    unsigned int temp = 0; 
+    int length = 16;
+    
+    for(int i = 0; i < length; i++){  //what if the length is corrupted? gotta check pfft.
+        if( i < 4) { 
+            temp |= (unsigned int) packet[i];            
+            if( i == 3){
+                temp >>= 4;
+                sourceAddress = temp; 
+                temp = 0;           
+            }
+            else{
+                temp <<= 8;
+            }     
+        }
+        else if( i < 7 ){  
+            if(i == 3){
+               temp |= (unsigned int) (packet[i] & 0x0f);   
+            }
+            else{
+                temp |= (unsigned int) packet[i];
+            }
+            if( i < 6){
+                temp <<= 8;
+            }
+            else{
+                destinationAddress = temp;
+                temp = 0; 
+            }           
+        }
+        else if(i == 7){
+            temp = packet[i];
+            temp >>= 4;
+            sourcePort = temp;
+            temp = 0;
+            temp |= (unsigned int) (packet[i] & 0x0f);  
+            destinationPort = temp;
+            temp = 0;
+        } 
+        else if(i < 10){ //needs recheck with different data 
+            temp |= (unsigned int) packet[i];
+            if( i == 9){
+                temp >>= 2;
+                fragmentOffset = temp; 
+                temp = 0;
+            }
+            else{
+                temp <<= 8;   
+            }
+        }
+        else if(i < 11){
+            continue;
+        }
+        else if( i < 13){
+            if( i == 11){
+                temp |= (unsigned int) (packet[i] & 0x0f);
+                temp <<= 8;
+            }
+            else{
+                temp |= (unsigned int) (packet[i] & 0x80);
+                temp >>= 7; 
+                maxHopCount = temp;
+                temp = 0;
+                temp |= (unsigned int) (packet[i] & 0x7f);
+                temp <<= 8;          
+            }
+        }
+        else if( i < 15){
+            temp |= (unsigned int) packet[i];                   
+            if( i == 14){     
+                checkSum = temp;                
+                temp = 0;
+            }
+            else{
+                temp <<= 8;  
+            }
+        }
+        else if( i == 15){
+            temp |= (unsigned int) (packet[i] & 0xC0);
+            temp >>= 6; 
+            compressionScheme = temp;
+            temp = 0;
+            temp |= (unsigned int) ( packet[i] & 0x3F);
+            trafficClass = temp;
+            temp = 0;     
+        }
+    }
+ }
+
+
+void decomposePayload(unsigned char packet[], int* payload, int payloadLength){
+    unsigned int temp = 0; 
+    int pktLen = (int) getPacketLength(packet);
+    int payLoadLength = getPayloadLength(packet);
+  
+   
+    if(payLoadLength != 0){
+        int payloadIndex = 0, bitCount = 0;
+        for(int i = 16; i < pktLen; i++){ //tentative algorithm. POTENTIAL VULNERABILITIES (Not checking checksum or looking for corrupted data)  
+            temp |= packet[i];
+            
+            bitCount+=8; 
+            if(bitCount == 32){
+                payload[payloadIndex] = (int) temp; 
+                temp = 0;
+                payloadIndex++;
+                bitCount = 0;
+            }
+            else {
+                temp <<= 8;
+            }
+        }
+    }
+}
+
+void print_packet_sf(unsigned char packet[]){
+
+    int pktLen = (int) getPacketLength(packet);
+    int payLoadLength = getPayloadLength(packet);
+    int payload[payLoadLength]; 
+    decomposeHeader(packet);
+    decomposePayload (packet, payload, payLoadLength);
+
+    printf("Source Address: %u\n", sourceAddress);
+    printf("Destination Address: %u\n", destinationAddress); 
+    printf("Source Port: %u\n", sourcePort); 
+    printf("Destination Port: %u\n", destinationPort); 
+    printf("Fragment Offset: %u\n", fragmentOffset); 
+    printf("Packet Length: %u\n", packetLength); 
+    printf("Max Hop Count: %u\n", maxHopCount); 
+    printf("CheckSum: %u\n", checkSum); 
+    printf("Compression Scheme: %u\n", compressionScheme); 
+    printf("Traffic Class: %u\n", trafficClass);   
+
+    printf("Payload: "); 
+    for(int i = 0; i < payLoadLength; i++){
+        printf("%d ", payload[i]);
+    }
+    printf("\n"); 
+   
+}
+
 
 int main() {
+    unsigned char packet[] = { 0x01, 0xd2, 0x08, 0xa0, 0xb4, 0x11, 0xaa, 0xcd, 
+                               0x00, 0x00, 0x01, 0xca, 0xde, 0xad, 0xb1, 0xf3, 
+                               0x00, 0x84, 0x5f, 0xed,
+                               0xff, 0xff, 0x66, 0x8f, 
+                               0x05, 0x88, 0x81, 0x92,};
+    int pktLen = (int) getPacketLength(packet);
+    int payLoadLength = getPayloadLength(packet);
+    int payload[payLoadLength]; 
+    decomposeHeader(packet);
+    decomposePayload (packet, payload, payLoadLength);
+    print_packet_sf(packet);
 
     return 0;
 }
+ 
